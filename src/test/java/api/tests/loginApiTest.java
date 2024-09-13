580a188f1535be62b95a3806f45ec2bfa476f3fa
aksh93 <akshaykumarpatil84@gmail.com>
2024-09-13 19:19:02 +0530

    new console added

--- src/test/java/api/tests/loginApiTest.java
+++ src/test/java/api/tests/loginApiTest.java
@@ -5,7 +5,7 @@ public class loginApiTest {
 	public void loginAPITest() {
 		// TODO Auto-generated method stub
 		System.out.println("running login API Test");
-
+		System.out.println("running login API Test 2");
 	}
 
 }
